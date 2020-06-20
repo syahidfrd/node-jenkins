4a08ee6184ec339ba70bee59fc402b9d642e92cf
Syahid Firdaus <syahidfrd@gmail.com>
2020-06-20 09:11:42 +0700

    feat: add deploy script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('new hello world');
 });
 
 app.listen(process.env.PORT || 3000);
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require('../index');
 
 describe('GET /', () => {
   it('should res hello world', (done) => {
-    request(app).get('/').expect('hello world', done);
+    request(app).get('/').expect('new hello world', done);
   });
 });
