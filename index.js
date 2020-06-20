5e7117d66a02029c90bfb545e40c569d3766bf47
Syahid Firdaus <syahidfrd@gmail.com>
2020-06-20 09:44:56 +0700

    feat: add deploy script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('new hello world2');
+  res.send('new hello');
 });
 
 app.listen(process.env.PORT || 3000);
