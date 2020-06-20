6fb56802da9fd9cfd5b0b2ec1dd90b8b19626ec9
Syahid Firdaus <syahidfrd@gmail.com>
2020-06-20 07:51:56 +0700

    feat: simple node app with test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /', () => {
+  it('should res hello world', (done) => {
+    request(app).get('/').expect('hello world', done);
+  });
+});
